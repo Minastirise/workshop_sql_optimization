7701f3720398e24cc4fd061056a6fd1f635f8af3
Minastirise <Minastirise@yandex.ru>
2022-09-19 09:49:15 +0300

    tech improvements

--- anaconda/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-apt update -y
-apt install -y python3-dev python3-pip libpq-dev gcc
-pip install psycopg2
-conda install jupyter -y --quiet 
-jupyter notebook --notebook-dir=/opt/notebooks --ip='*' --port=30888 --no-browser --allow-root
