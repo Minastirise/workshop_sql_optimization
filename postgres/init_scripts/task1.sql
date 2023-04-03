cf180394eb15030f1aff0035c6cb7fbb0de70113
Svetlana Moskvina <Minastirise@yandex.ru>
2023-04-03 18:23:39 +0300

    Update info and add partitioning

--- postgres/init_scripts/task1.sql
+++ postgres/init_scripts/task1.sql
@@ -19,3 +19,11 @@ INSERT INTO task1
     	floor(random() * 100000 + 1)::int,
 		case when floor(random() * 20 + 1)::int = 1 then 'click' else 'show' end,
 		timestamp '2022-05-20 00:00:00' +  random() * interval '3 days';
+
+create table task11 (event_id int, user_id int, type varchar, dt timestamptz);
+
+INSERT INTO task11
+	SELECT generate_series(1,10000000),
+    	floor(random() * 100000 + 1)::int,
+		case when floor(random() * 20 + 1)::int = 1 then 'click' else 'show' end,
+		timestamp '2022-05-20 00:00:00' +  random() * interval '3 days';
